333126bf30fd627c63f3a2f7ab8c3233ec9f52f7
Damian Chojnacki <daamian3@gmail.com>
2025-05-23 19:49:45 +0200

    fix: add id to task resource

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -18,6 +18,7 @@ class TaskResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'description' => $this->description,
             'status' => $this->status->name,
